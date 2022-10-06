b2d64b20e504eed3b6cb2424d46b0c24a7f7f5f8
LeeKiller9 <112848883+LeeKiller9@users.noreply.github.com>
2022-10-07 00:00:04 +0700

    upfiles

--- src/ConvertNumberToText.java
+++ src/ConvertNumberToText.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class ConvertNumberToText {
     public static void main(String[] args) {
         int number;
@@ -7,48 +6,47 @@ public class ConvertNumberToText {
         System.out.println("Enter number: ");
         number = scanner.nextInt();
         String[] ar1 = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        String[] ar2 = {"", "", "", "third", "four", "fif", "six", "seven", "eigh", "nine"};
-        String dvi;
-        String chuc;
-        String tram;
+        String[] ar2 = {"", "", "", "thir", "four", "fif", "six", "seven", "eigh", "nine"};
         int numDvi;
         int numChuc;
         int numTram;
         String result = "";
-        switch (number) {
-            case 0:
-                result = "zero";
-                break;
-            case 10:
-                result = "ten";
-                break;
-            case 11:
-                result = "eleven";
-                break;
-            case 12:
-                result = "twelve";
-                break;
-            case 14:
-                result = "fourteen";
-                break;
-        }
-        if (result.equals("") && number >= 0) {
-            if (number < 10) {
-                result = ar1[number];
-            } else if (number < 100) {
-                numChuc = number / 10;
-                numDvi = number - numChuc * 10;
-                if (numChuc == 1) {
-                    result = ar2[numDvi] + "teen";
-                } else if (numChuc == 2) {
-                    chuc = "twenty";
-                    result = chuc + " " + ar1[numDvi];
-                } else if (numChuc >= 3) {
-                    chuc = ar2[numChuc];
-                    result = chuc + "ty " + ar1[numDvi];
+        numTram = number / 100;
+        numChuc = (number - numTram * 100) / 10;
+        numDvi = number - numTram * 100 - numChuc * 10;
+        if (number == 0) {
+            result += "zero";
+        } else {
+            if (numTram > 0) {
+                result += ar1[numTram] + " hundred ";
+            }
+            if (numChuc == 0) {
+                result += ar1[numDvi];
+            } else {
+                switch (numChuc * 10 + numDvi) {
+                    case 10:
+                        result += "ten";
+                        break;
+                    case 11:
+                        result += "eleven";
+                        break;
+                    case 12:
+                        result += "twelve";
+                        break;
+                    case 14:
+                        result += "fourteen";
+                        break;
+                    default:
+                        if (numChuc == 1) {
+                            result += ar2[numDvi] + "teen";
+                        } else if (numChuc == 2) {
+                            result += "twenty " + ar1[numDvi];
+                        } else {
+                            result += ar2[numChuc] + "ty " + ar1[numDvi];
+                        }
                 }
             }
-            System.out.printf("%d convert to text is: %s", number, result);
         }
+        System.out.printf("%d convert to text is: %s", number, result);
     }
 }
